844b965ab3fbcf78b93ec38db44e39bb395df98c
Tlams <contact@thomasgsp.fr>
2018-02-09 12:18:10 +0000

    locker fix issue

--- code/scripts/main/core/libs/locker.py
+++ code/scripts/main/core/libs/locker.py
@@ -1,19 +1,20 @@
 import os
 
 class Locker:
-    def createlock(self, lockfile, date=0):
+    def createlock(self, lockfile, position, date=0):
         try:
             if os.path.exists(lockfile):
                 f = open(lockfile, "r+")
                 return f.read()
             else:
                 f = open(lockfile, "w")
-                f.write(date)
+                f.write(str(date))
         except BaseException as e:
             raise("Can't create or read the lock file: {0}".format(e))
 
-    def unlock(self, lockfile):
+    def unlock(self, lockfile, position):
         try:
             os.remove(lockfile)
         except BaseException as e:
-            raise ("Can't delete the lock file: {0}".format(e))
+            if position != "startup":
+                print("Can't delete the lock file: {0}".format(e))
