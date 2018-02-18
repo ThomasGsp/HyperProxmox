97467fe227dc0e40170bfadc4dcc099633b25f12
Tlams <contact@thomasgsp.fr>
2018-02-18 18:21:09 +0000

    Change defaults values

--- code/web/www/pages/requires/configs.php
+++ code/web/www/pages/requires/configs.php
@@ -6,8 +6,8 @@ $LANG = "EN";
 $baseurl = "http://www.eyeprox.lan";
 
 
-$sto_regx_status = false; // false = default on the size
-$sto_regx = "/((^VMs)|(!VG)).+/i"; // if not found, use size
+$sto_regx_status = true; // false = default on the size
+$sto_regx = "/disks/i"; // if not found, use size
 
 $non_grata_weight = 1000;
 
