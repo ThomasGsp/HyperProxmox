095519ed06efd0848811943451c3574c2330cdfc
Tlams <contact@thomasgsp.fr>
2018-02-18 18:14:03 +0000

    Fix HTML issues

--- code/web/www/pages/includes/footer.php
+++ code/web/www/pages/includes/footer.php
@@ -1,7 +1,4 @@
 
-        </div>
-
-        <!-- <center> VM viewer and management center (VM-VMC) -- V0.1 by Tlams</center> -->
         <center> PCM - Proxmox Central Management -- V0.1 by Tlams</center>
     </body>
 
