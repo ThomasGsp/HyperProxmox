045274ca6d6f23a77668d13a2476bd1c17b3a604
Tlams <contact@thomasgsp.fr>
2018-02-19 18:17:02 +0000

    Add/commit page vmactions

---
+++ code/web/www/pages/actionvm.php
@@ -0,0 +1,50 @@
+<?php
+
+include(dirname(__DIR__).'/pages/includes/header.php');
+
+$instanceinfo = json_decode($q->GET_byid("instances", $_GET['id']), true)['value'];
+$instanceinfo = (object) $instanceinfo;
+
+$status = json_decode($q->GET_status('current', $_GET['id']), true)['value']['data'];
+$status = (object) $status;    
+?>
+<div id="page-wrapper">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header"> VM - Informations & actions </h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body" >
+                   	<ul>
+                		<li> Cluster: <?php echo  $instanceinfo->cluster; ?> </li>
+                    	<li> Node: <?php echo  $instanceinfo->node; ?> </li>
+                    	<li> VM: <?php echo  $instanceinfo->name; ?> </li>
+                    	<li> ID: <?php echo  $instanceinfo->vmid; ?> </li>
+                	</ul>
+                	
+                    <select id="action" class="selectaction" data-width="auto" >
+                    	<option  value="">Choice </option>
+                            <option value="current">Status</option>
+                            <option value="start">Start</option>
+                            <option value="stop">Stop</option>
+                            <option value="shutdown">Shutdown</option>
+                            <option value="reset">Reset</option>
+                        </optgroup>
+                    </select>
+		             <button id="bbaction" value="<?php echo  $instanceinfo->_id["\$oid"]; ?>">Click me</button> 
+                </div>
+ 				<div class="result"></div>
+            </div>
+            
+            <?php echo json_encode($status, JSON_PRETTY_PRINT); ?>
+        </div>
+    </div>
+    <?php
+    include(dirname(__DIR__).'/pages/includes/footer.php');
+    ?>
